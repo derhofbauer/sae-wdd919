329cb9727070c5e580057f4e9d9cdd5f63329501
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2020-09-15 20:43:28 +0200

    [TASK] Übung 1, Abschnitt 1
    Signed-off-by: Alexander Hofbauer <hofbauer.alexander@gmail.com>

---
+++ Übungen/1.1/contact.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'header.php';
+require_once 'nav.php';
+?>
+Contact
+<?php require_once 'footer.php'; ?>
+
---
+++ Übungen/1.1/nav.php
@@ -0,0 +1,9 @@
+<div>Header!
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="blog.php">Blog</a></li>
+        </ul>
+    </nav>
+</div>
