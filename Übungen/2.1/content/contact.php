104c5c14140d5672343cec37be21dd1ddd2f23bd
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2020-09-22 19:38:06 +0200

    [FEATURE] finish 2.1

--- Übungen/2.1/content/contact.php
+++ Übungen/2.1/content/contact.php
@@ -1,28 +1,43 @@
 <style>
-    label {
-      display: block;
-    }
+  label {
+    display: block;
+  }
 </style>
 
 <main>
     <form action="index.php?page=contact-validate" method="post" novalidate>
         <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" name="name" id="name">
+            <input type="text" name="name" id="name" value="<?php echo $_POST['name']; ?>">
         </div>
         <div class="form-group">
             <label for="email">E-Mail</label>
-            <input type="email" name="email" id="email">
+            <input type="email" name="email" id="email" value="<?php echo $_POST['email']; ?>">
         </div>
         <div class="form-group">
             <label for="gender">Geschlecht</label>
-            <label><input type="radio" name="gender" value="f"> weiblich</label>
-            <label><input type="radio" name="gender" value="m"> männlich</label>
-            <label><input type="radio" name="gender" value="nb"> non-binary</label>
+            <?php
+            $genders = [
+                'm' => 'weiblich',
+                'f' => 'männlich',
+                'nb' => 'non-binary'
+            ];
+
+            foreach ($genders as $htmlValue => $label): ?>
+                <label>
+                    <?php
+                    $checkedParticle = '';
+                    if ($_POST['gender'] === $htmlValue) {
+                        $checkedParticle = ' checked';
+                    }
+                    ?>
+                    <input type="radio" name="gender" value="<?php echo $htmlValue; ?>"<?php echo $checkedParticle; ?>> <?php echo $label; ?>
+                </label>
+            <?php endforeach; ?>
         </div>
         <div class="form-group">
             <label for="message">Nachricht</label>
-            <textarea name="message" id="message" cols="30" rows="5"></textarea>
+            <textarea name="message" id="message" cols="30" rows="5"><?php echo $_POST['message']; ?></textarea>
         </div>
         <div class="form-group">
             <label for="newsletter">Newsletter?</label>
--- Übungen/2.1/validators/contact-validate.php
+++ Übungen/2.1/validators/contact-validate.php
@@ -1,3 +1,34 @@
 <?php
 
-var_dump('validate');
+$hasErrorsSwitch = false;
+
+if (strlen($_POST['name']) < 2) {
+    echo "<p>Der Name muss mindestens 2 Zeichen lang sein</p>";
+    $hasErrorsSwitch = true;
+}
+if (strpos($_POST['email'], '@') === false) {
+    echo "<p>Die E-Mail Adresse muss ein @ beinhalten</p>";
+    $hasErrorsSwitch = true;
+}
+if (strpos($_POST['email'], '.') === false) {
+    echo "<p>Die E-Mail Adresse muss einen . beinhalten</p>";
+    $hasErrorsSwitch = true;
+}
+$genders = ['m', 'f', 'nb'];
+if (isset($_POST['gender']) !== true || in_array($_POST['gender'], $genders) === false) {
+    echo "<p>Bitte ein Geschlecht aus der Liste auswählen</p>";
+    $hasErrorsSwitch = true;
+}
+if (strlen($_POST['message']) < 10) {
+    echo "<p>Bitte geben Sie eine odentliche Nachricht ein, ich mein, was soll das?!</p>";
+    $hasErrorsSwitch = true;
+}
+if (isset($_POST['newsletter']) && $_POST['newsletter'] === 'on') {
+    echo "<p>Newsletter Anmeldung erfolgreich! :D</p>";
+}
+
+if ($hasErrorsSwitch === true) {
+    require_once __DIR__ . '/../content/contact.php';
+} else {
+    require_once __DIR__ . '/../content/thank-you.php';
+}
