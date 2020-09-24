6b1db8a586486349f45f94f6bc72a280a81ba7c3
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2020-09-24 20:36:03 +0200

    [FEATURE] finish 3.2

---
+++ Übungen/3.2/partials/nav.php
@@ -0,0 +1,10 @@
+<div>Header!
+    <nav>
+        <ul>
+            <li><a href="index.php?page=home">Home</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
+            <li><a href="index.php?page=blog">Blog</a></li>
+            <li><a href="index.php?page=links">Links</a></li>
+        </ul>
+    </nav>
+</div>
