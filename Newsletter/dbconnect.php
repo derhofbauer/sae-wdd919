508c354596f8423f83c9d90e08a766811e51dc14
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2020-09-29 20:16:35 +0200

    [FEATURE] start newsletter

---
+++ Newsletter/dbconnect.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * https://www.php.net/manual/de/function.mysqli-connect.php
+ */
+$link = mysqli_connect('localhost', 'root', 'secret', 'newsletter');
---
+++ Newsletter/index.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Todo
+ *
+ * [ ] Mehrere Topics
+ * [ ] Email + Name speichern
+ * [ ] Abos-Übersicht
+ * [ ] Übersicht über alle Topics
+ * [ ] Abo-Formular
+ *
+ * ----
+ *
+ * [x] Tabellen definieren (Tabellen: Users, Abos, Topics)
+ * [x] Spalten definieren User: ID, Nachname, Vorname, E-Mail
+ * [x] Spalten definieren Abo: ID, Topic ID, User ID
+ * [x] Spalten definieren Topic: ID, Name, Beschreibung
+ *
+ */
+
+require_once 'dbconnect.php';
+
+var_dump($link);
+
+require_once 'partials/header.php';
+?>
+
+<div class="container">
+    <div class="row topics">
+        <?php
+        /**
+         * [ ] Liste der Topics
+         */
+        ?>
+    </div>
+    <div class="row form">
+        <?php
+        /**
+         * Abo-Formular
+         * [ ] Auswahl der Topics
+         * [ ] Email, Vorname, Nachname
+         */
+        ?>
+    </div>
+</div>
+
+<?php
+require_once 'partials/footer.php';
+?>
